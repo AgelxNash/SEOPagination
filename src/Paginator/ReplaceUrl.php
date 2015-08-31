19dfd50fb61e89231b16b7e17b39bf04fc08aa5b
AgelxNash <agel_nash@xaker.ru>
2015-08-31 13:23:54 +0500

    Fix #6

--- src/Paginator/ReplaceUrl.php
+++ src/Paginator/ReplaceUrl.php
@@ -35,10 +35,9 @@ trait ReplaceUrl{
 
 	public function checkPaginate($keepQuery = null){
 		$flag = true;
-
 		$request = Request::get($this->getPageName());
 		$cPage = $this->currentPage();
-		if(1 == $cPage) $this->setActionOnError('first');
+		if(1 == $cPage || !method_exists($this, 'lastPage')) $this->setActionOnError('first');
 		if((1 == (int)$request) || ($this->isEmpty() && 1!=$cPage) || (1==$cPage && !is_null($request) && (int)$request!=$cPage)){
 			if(is_null($keepQuery)){
 				$keepQuery = $this->getKeepQuery();
